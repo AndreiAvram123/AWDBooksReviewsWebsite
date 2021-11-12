50c299ff64618f56e97fe68dbee3110704355193
Andrei Avram <avramandreitiberiu@gmail.com>
2021-11-12 20:04:45 +0000

    Added bookReview and navbar

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\BookReview;
 use App\Form\BookReviewType;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,14 +12,40 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class BookController extends AbstractController
 {
+
+    #[Route("/", name: "home", methods: ["GET"])]
+    public function index(): Response{
+        $repo = $this->getManager()->getRepository(BookReview::class);
+        $allReviews = $repo->findAll();
+        return $this->render('book/index.html.twig', [
+            'allReviews' => $allReviews
+        ]);
+    }
+
+
+
     #[Route('/book/{id}', name : "get_book_by_id",methods: ["GET","POST"])]
-    public function index(Request $request): Response
+    public function displayBookById(Request $request): Response
     {
         $bookReview = new BookReview();
         $form = $this->createForm(BookReviewType::class, $bookReview);
+        if($form->isSubmitted() && $form-> isValid()){
+            $bookReview = $form->handleRequest($request);
+            $this->persistAndFlush($bookReview);
+            return $this->redirect("/");
+        }
         return $this-> renderForm('book/index.html.twig',[
             'form' => $form
             ]
         );
+    }
+
+    private function getManager(): ObjectManager
+    {
+        return $this->getDoctrine()->getManager();
+    }
+    private function persistAndFlush($object){
+        $this->getManager()->persist($object);
+        $this->getManager()->flush();
     }
 }
