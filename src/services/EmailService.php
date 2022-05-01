d2eb9e5d2c8e32a115b2f8b4e4191490d0fe559d
Andrei Avram <avramandreitiberiu@gmail.com>
2022-05-01 22:30:47 +0100

    Remove email verification functionality. Could not be implemented on the website

--- src/services/EmailService.php
+++ src/services/EmailService.php
@@ -10,11 +10,9 @@ use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use GuzzleHttp\Client;
 use JMS\Serializer\SerializerInterface;
-use SendGrid\Mail\Mail;
 
 class EmailService
 {
-    private \SendGrid $sendGrid;
     private string $fromIdentity = "a.t.avram@edu.salford.ac.uk";
     private string $confirmationEmailTemplateID = "d-2c206b86f8014d88b5e1557ac0993550";
     private string $newReviewTemplate = "d-83e60e79c21942c1b04343c9b07ebb52";
@@ -25,12 +23,9 @@ class EmailService
 
     public function __construct(
         private string $sendGridApiKey,
-        private string $verificationURL,
-        private EntityManagerInterface $entityManager,
         private SerializerInterface $serializer
 
     ){
-        $this->sendGrid = new \SendGrid($sendGridApiKey);
         $this->client = new Client(['base_uri' => self::baseUrl]);
     }
 
@@ -64,62 +59,4 @@ class EmailService
                 'body' => $emailDataJson
             ]);
     }
-
-
-
-    public function sendConfirmationEmail(User $user){
-        $email = new Mail();
-        $email->setFrom($this->fromIdentity);
-        $email->setTemplateId($this->confirmationEmailTemplateID);
-        $uuid = uniqid();
-        $email->addDynamicTemplateData(
-            key: "confirmationLink",
-            value: $this->verificationURL . $uuid
-        );
-        $email->addTo(to : $user->getEmail());
-        try{
-            $response = $this->sendGrid->send($email);
-            if($response->statusCode()> 200 && $response->statusCode() < 400){
-                $this->persistEmailValidation(
-                    uuid: $uuid,
-                    user: $user
-                );
-            }
-        }catch (Exception $exception){
-
-        }
-    }
-
-
-    private function persistEmailValidation(
-        string $uuid,
-        User $user
-    ):EmailValidation{
-        $emailValidation = new EmailValidation();
-        $emailValidation->setUser($user);
-        $emailValidation->setUuid($uuid);
-        $now = new \DateTimeImmutable();
-        $expirationDate = $now->modify('+1 day');
-        $emailValidation->setExpirationDate($expirationDate);
-        $this->entityManager->persist($emailValidation);
-        $this->entityManager->flush();
-        return $emailValidation;
-    }
-
-
-    public function setEmailValidated(
-        EmailValidation $emailValidation
-    ){
-        $emailValidation->getUser()->setIsEmailVerified(true);
-        $this->entityManager->remove($emailValidation);
-        $this->entityManager->flush();
-    }
-
-    public function removeExpiredVerification(
-        EmailValidation $emailValidation
-    ){
-        $this->entityManager->remove($emailValidation);
-        $this->entityManager->flush();
-    }
-
 }
